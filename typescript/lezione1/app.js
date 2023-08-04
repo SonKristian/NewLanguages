27319841e7e5f8ddd15c41356e3ca9a1725682ed
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-08-04 12:12:58 +0200

    lezione 1

---
+++ typescript/lezione1/app.js
@@ -0,0 +1,13 @@
+//   function somma(a, b) {
+//     if (typeof a === "number" && typeof b === "number") {
+//       return a + b;
+//     } else {
+//       return "somma non possibile";
+//     }
+//   }
+//   console.log(somma(3, 5));
+function somma(a, b) {
+    return a + b;
+}
+// console.log(somma(3,"5"))
+console.log(somma(3, 5));
---
+++ typescript/lezione1/app.ts
@@ -0,0 +1,14 @@
+//   function somma(a, b) {
+//     if (typeof a === "number" && typeof b === "number") {
+//       return a + b;
+//     } else {
+//       return "somma non possibile";
+//     }
+//   }
+//   console.log(somma(3, 5));
+
+function somma(a:number, b:number){
+    return a+b
+}
+// console.log(somma(3,"5"))
+console.log(somma(3,5))
